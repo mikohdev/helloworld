db8a53556b4f923e2d8820a2f6ac1bf3ee1728df
Miko <miko@MikonoMacBook-Pro.local>
2022-08-18 15:35:11 +0900

    Updates

---
+++ precache-manifest.83e8f1bac4eb5e036e1049bde1acae52.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "5c790a1b740580835be3",
+    "url": "/helloworld/static/css/main.effee078.chunk.css"
+  },
+  {
+    "revision": "5c790a1b740580835be3",
+    "url": "/helloworld/static/js/main.59ccc0ee.chunk.js"
+  },
+  {
+    "revision": "60c9b248e2e673e074c3",
+    "url": "/helloworld/static/js/runtime~main.da371a62.js"
+  },
+  {
+    "revision": "e634b1ff2fe29577199e",
+    "url": "/helloworld/static/css/2.ed833b43.chunk.css"
+  },
+  {
+    "revision": "e634b1ff2fe29577199e",
+    "url": "/helloworld/static/js/2.fd0ca411.chunk.js"
+  },
+  {
+    "revision": "440f8641b4ac1935df7a",
+    "url": "/helloworld/static/js/3.76c3d8c6.chunk.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/helloworld/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "da00884413b5a45cb4aaca4ccb166579",
+    "url": "/helloworld/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.cf5af6a6c7f97ec9966d8ac7811d58ab.js"
+  "/helloworld/precache-manifest.83e8f1bac4eb5e036e1049bde1acae52.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/helloworld/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
